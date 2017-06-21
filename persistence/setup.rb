5bb384762e77e80d18090789d15345445aeca57c
Tim Riley <tim@openmonkey.com>
2017-06-21 11:22:04 +0800

    Update persistence setup

--- persistence/setup.rb
+++ persistence/setup.rb
@@ -3,7 +3,7 @@ require "rom/sql"
 
 module Persistence
   def self.config
-    @config ||= ROM::Configuration.new(:sql, "sqlite::memory").tap do |config|
+    @config ||= ROM::Configuration.new(:sql, "postgres://localhost/persistence_exercises").tap do |config|
       config.plugin :sql, relations: :auto_restrictions
     end
   end
@@ -17,10 +17,18 @@ module Persistence
     @rom
   end
 
-  def self.finalize!
+  def self.finalize
     @rom ||= begin
       config.auto_registration File.join(__dir__, "lib/persistence")
       ROM.container(config)
     end
+  end
+
+  def self.reset_with_migration(migration)
+    db.connection.tables.each do |table|
+      db.connection.drop_table table, cascade: true
+    end
+
+    migration.apply db.connection, :up
   end
 end
